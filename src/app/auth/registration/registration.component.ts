169b1fba850dfd7fccd87712951bd4dbda429394
aarver <bacsajozseff@gmail.com>
2023-05-19 11:52:10 +0200

    Elkészült a registration form validáció

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -13,24 +13,57 @@ export class RegistrationComponent {
 
 
 
+
+
   constructor(private authService: AuthService, private snackBar: MatSnackBar) {
     this.registerForm = new FormGroup({
-      email: new FormControl('',),
-      password: new FormControl('',),
-      passwordAgain: new FormControl('',)
-    });
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)] ),
+      passwordAgain: new FormControl('', Validators.required, )
+    },
+      {
+        validators: this.passwordMatch('password','passwordAgain')
+      }
+  
+    );
   }
 
   send(form: FormGroup) {
     this.registerForm.markAllAsTouched();
-    this.authService.registerUser({ ...form.value, })
+    this.authService.registerUser({ ...form.value, role: 'user' })
     form.reset();
-    
-    this.snackBar.open('Regitráció sikeres!', 'Bezárás',{
+
+    this.snackBar.open('Regitráció sikeres!', 'Bezárás', {
       duration: 5000,
     });
+  }
+
+  passwordMatch(password: string, confirmPassword: string): ValidatorFn {
+    return (formGroup: AbstractControl): { [key: string]: any } | null => {
+      const passwordControl = formGroup.get(password);
+      const confirmPasswordControl = formGroup.get(confirmPassword);
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return null;
+      }
+      if (
+        confirmPasswordControl.errors &&
+        !confirmPasswordControl.errors['passwordMismatch']
+      ) {
+        return null;
+      }
+
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+        return { passwordMismatch: true };
+      } else {
+        confirmPasswordControl.setErrors(null);
+        return null;
+      }
+    };
+  }
 
   }
 
 
-}
+
